e27778e4b5206374b731bd4239aa1d250ac5738e
KOT <tedoturmanidze12@gmail.com>
2019-12-07 12:43:59 +0300

    rangers

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log (Math.round = 2.33123123) c11
+
+
+console.log (Math.round = 2.33123123) console.log (Math.round = 2.33123123) 
+console.log (Math.round = 2.33123123) 
+console.log (Math.round = 2.33123123) 
+console.log (Math.round = 2.33123123) 
+console.log (Math.round = 2.33123123) 
+console.log (Math.round = 2.33123123) 
