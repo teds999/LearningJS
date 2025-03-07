be9599247d9c03b037ba8e2cf4713e43d79ce31e
KOT <tedoturmanidze12@gmail.com>
2025-03-07 15:43:07 +0300

    chechk about discount

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-console.log('Wellcome to Programing World !');
+
+const userIsAdmin = false ;
 
 
-console.log(5+5);
+const discout = userIsAdmin ? 25 : 0 ;
 
 
-console.log(123*3123123);
-
-let x = 5;
-console.log(x+12*3);
+console.log(discout);
