bceffbeb5f15de731891ab59de1e77cb996fa7ea
KOT <tedoturmanidze12@gmail.com>
2025-03-08 21:37:36 +0300

    low kick

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log (Math.round = 2.33123123);
 console.log(123123);
+console.log (Math.round = 2.33123123);
+console.log(123123);
+console.log (Math.round = 2.33123123);
+console.log(123123);
+console.log (Math.round = 2.33123123);
+console.log(123123);
+console.log (Math.round = 2.33123123);
